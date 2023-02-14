5ba8a33e95d97834e03646cc30f99551f06fd690
tlmacedo <tl.macedo@hotmail.com>
2023-02-13 22:47:09 -0400

    refatoracao de codigo

--- formacao/python/orientacao_a_objetos/jogos/adivinhacao.py
+++ formacao/python/orientacao_a_objetos/jogos/adivinhacao.py
@@ -2,55 +2,65 @@ import random
 
 
 def jogar():
-    print("*********************************")
-    print("Bem vindo ao jogo de Adivinhação!")
-    print("*********************************")
+    print("*************************************************")
+    print("******* Bem vindo ao jogo de Adivinhação! *******")
+    print("*************************************************")
 
-    numero_secreto = random.randrange(1, 101)
-    print("número: {}".format(int(numero_secreto)))
-    total_de_tentativas: int = 0
+    pontos_inicial: int = 1000
+    numero_min: int = 1
+    numero_max: int = 100
+    numero_secreto: int = random.randrange(numero_min, numero_max + 1)
+    niveis_de_dificuldade = [('Facil', 20), ('Medio', 10), ('Dificil', 5)]
+    nivel_min: int = 1
+    nivel_max: int = len(niveis_de_dificuldade) + 1
+    nivel_dificuldade: int = 0
+    nivel_dificuldade_index: int
+    chute: int
+    rodada: int = 1
+    strNivel: str
+    strChute: str
 
-    pontos = 1000
+    niveis_de_dificuldade_exibicao: str = ''
+    for i in niveis_de_dificuldade:
+        niveis_de_dificuldade_exibicao += '({})-{}  '.format(niveis_de_dificuldade.index(i) + 1, i[0])
 
-    print("Qual nível de dificuldade ?")
-    print("(1)-Fácil (2)-Médio (3)-Difícil")
-
-    nivel = int(input("Defina o nível: "))
-    while nivel < 1 or nivel > 3:
-        nivel = int(input("Nível {} não é válido, defina um nível válido: ".format(nivel)))
-
-    match nivel:
-        case 1:
-            total_de_tentativas = 20
-        case 2:
-            total_de_tentativas = 10
-        case 3:
-            total_de_tentativas = 5
-        case _:
-            print("nível não existe!\ninicie novamente")
-            exit(0)
-
-    for rodada in range(1, total_de_tentativas + 1):
-        print("Tentativa, {} de {}.".format(rodada, total_de_tentativas))
-        chute = int(input("Digite o número entre 1 e 100: "))
-        print("Você digitou", chute)
-
-        if chute < 1 or chute > 100:
-            print("Você deve digitar um número entre 1 e 100!")
+    while nivel_dificuldade not in range(nivel_min, nivel_max):
+        if nivel_dificuldade is None:
+            strNivel = 'Níveis de dificuldade: {}\nQual nível vc quer jogar ? dígite o número referente ao nível ' \
+                       'escolhido: ' \
+                .format(niveis_de_dificuldade_exibicao)
         else:
-            acertou = (chute == numero_secreto)
+            strNivel = "Nível {} não é válido, defina um nível válido: ".format(nivel_dificuldade)
 
-            if acertou:
-                print("Você acertou e fez {} pontos!".format(pontos))
-                break
-            else:
-                maior = (chute > numero_secreto)
-                menor = (chute < numero_secreto)
-                print("Você errou! O seu chute foi {} do que o número secreto.".format("maior" if maior else "menor"))
-                pontos_perdidos = abs(numero_secreto - chute)
-                pontos -= pontos_perdidos
+        nivel_dificuldade = int(input(strNivel).strip()[:1])
+    nivel_dificuldade_index = nivel_dificuldade - 1
 
-    print("Fim do jogo")
+    total_tentativas = niveis_de_dificuldade[nivel_dificuldade_index][1]
+    print(
+        "\nParabéns o nível de dificuldade escolhido foi o '{}' e vc vai ter [{}] tentativas.\nSua pontuação inicial "
+        "é {} pontos".format(niveis_de_dificuldade[nivel_dificuldade_index][0], total_tentativas, pontos_inicial))
+
+    for jogadas in range(rodada, total_tentativas + 1):
+        chute = int(input('Tentativa [{}] de [{}].\nDigite um número entre {} e {}: '
+                          .format(jogadas, total_tentativas, numero_min, numero_max)).strip())
+        while chute not in range(numero_min, numero_max + 1):
+            chute = int(
+                input('Número [{}] não é válido para o jogo tente um número entre [{}] e [{}]... tente novamente: '
+                      .format(chute, numero_min, numero_max)).strip())
+
+        acertou = (chute == numero_secreto)
+
+        if acertou:
+            print("Você acertou e fez {} pontos!".format(pontos_inicial))
+            break
+        else:
+            maior = (chute > numero_secreto)
+            menor = (chute < numero_secreto)
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos_inicial -= pontos_perdidos
+            print("Você errou! O seu chute foi {} do que o número secreto.".format("maior" if maior else "menor"))
+        print('Sua atual pontuação é de {} pontos.'.format(pontos_inicial))
+    print("\nFim do jogo")
 
 
 if __name__ == "__main__":
