e53d81750e46dd6982c88b0bebc2c8e8b4da45fa
tlmacedo <tl.macedo@hotmail.com>
2023-02-13 23:35:58 -0400

    organizando o codigo em funcoes.

--- formacao/python/orientacao_a_objetos/jogos/forca.py
+++ formacao/python/orientacao_a_objetos/jogos/forca.py
@@ -1,18 +1,16 @@
 import random
 
 
-def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo de Forca!***")
-    print("*********************************")
+def inicializa_letras_acertadas(palavra):
+    return ['_' for i in palavra]
 
-    arquivo = open('palavras.txt', 'r')
-    palavras = []
-    for linha in arquivo:
-        palavras.append(linha.strip())
-    arquivo.close()
-    palavra_secreta = palavras[random.randrange(0, len(palavras))]
-    palavra_forca = ['_' for i in palavra_secreta]
+
+def jogar():
+    imprime_mensagem_abertura()
+
+    palavra_secreta = escolhe_palavra_secreta()
+
+    palavra_forca = inicializa_letras_acertadas(palavra_secreta)
     chutes = []
     enforcou = False
     acertou = False
@@ -60,3 +58,19 @@ def jogar():
 
 if __name__ == "__main__":
     jogar()
+
+
+def imprime_mensagem_abertura():
+    print("*********************************")
+    print("***Bem vindo ao jogo de Forca!***")
+    print("*********************************")
+
+
+def escolhe_palavra_secreta():
+    arquivo = open('palavras.txt', 'r')
+    palavras = []
+    for linha in arquivo:
+        palavras.append(linha.strip())
+    arquivo.close()
+    palavra_secreta = palavras[random.randrange(0, len(palavras))]
+    return palavra_secreta
