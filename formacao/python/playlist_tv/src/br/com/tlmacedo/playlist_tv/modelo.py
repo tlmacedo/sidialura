792015ba6296cfa99d3c10e5c62ced23f0ac21d2
tlmacedo <tl.macedo@hotmail.com>
2023-02-22 18:55:04 -0400

    duplicação de comportamento

--- formacao/python/playlist_tv/src/br/com/tlmacedo/playlist_tv/modelo.py
+++ formacao/python/playlist_tv/src/br/com/tlmacedo/playlist_tv/modelo.py
@@ -1,21 +1,42 @@
 class Filme:
 	def __init__(self, nome, ano, duracao):
 		print('criando {}...'.format(self.__class__.__name__))
-		self.__nome = nome
+		self.nome = nome
 		self.__ano = ano
 		self.__duracao = duracao
+		self.__likes = 0
 
 	@property
 	def nome(self):
 		return self.__nome
 
+	@nome.setter
+	def nome(self, nome):
+		self.__nome = nome.title()
+
+	@property
+	def ano(self):
+		return self.__ano
+
+	@property
+	def duracao(self):
+		return self.__duracao
+
+	@property
+	def likes(self):
+		return self.__likes
+
+	def dar_like(self):
+		self.__likes += 1
+
 
 class Serie:
 	def __init__(self, nome, ano, temporadas):
 		print('criando {}...'.format(self.__class__.__name__))
-		self.__nome = nome
+		self.__nome = nome.title()
 		self.__ano = ano
 		self.__temporadas = temporadas
+		self.__likes = 0
 
 	@property
 	def nome(self):
@@ -28,3 +49,10 @@ class Serie:
 	@property
 	def temporadas(self):
 		return self.__temporadas
+
+	@property
+	def likes(self):
+		return self.__likes
+
+	def dar_likes(self):
+		self.__likes += 1
