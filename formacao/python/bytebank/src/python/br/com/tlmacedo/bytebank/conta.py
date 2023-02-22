f9dcd37d22d26f334d918eda45d0f5ee3fb5992b
tlmacedo <tl.macedo@hotmail.com>
2023-02-22 16:14:29 -0400

    Alterando atributos privados

--- formacao/python/bytebank/src/python/br/com/tlmacedo/bytebank/conta.py
+++ formacao/python/bytebank/src/python/br/com/tlmacedo/bytebank/conta.py
@@ -5,16 +5,48 @@ class Conta:
 		self.__numero = numero
 		self.__titular = titular
 		self.__saldo = saldo
-		self.__limite = limite
+		self.___limite = limite
 
-	def extrato(self):
-		print('Saldo de {} do titular: {}'.format(self.__saldo, self.__titular))
+	@property
+	def numero(self):
+		return self.__numero
+
+	@numero.setter
+	def numero(self, numero):
+		self.__numero = numero
+
+	@property
+	def titular(self):
+		return self.__titular
+
+	@titular.setter
+	def titular(self, titular):
+		self.__titular = titular
+
+	@property
+	def saldo(self):
+		return self.__saldo
+
+	@saldo.setter
+	def saldo(self, saldo):
+		self.__saldo = saldo
+
+	@property
+	def limite(self):
+		return self.__limite
+
+	@limite.setter
+	def limite(self, limite):
+		self.__limite = limite
 
 	def depositar(self, valor):
 		self.__saldo += valor
 
 	def sacar(self, valor):
 		self.__saldo -= valor
+
+	def extrato(self):
+		print('Saldo de {} do titular: {}'.format(self.__saldo, self.__titular))
 
 	def transferir(self, valor, conta_destino):
 		self.sacar(valor)
