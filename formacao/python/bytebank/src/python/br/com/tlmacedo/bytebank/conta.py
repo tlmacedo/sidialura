4b1a1eef815aef568dc1b09ab8f597381c7bbe3e
tlmacedo <tl.macedo@hotmail.com>
2023-02-22 16:16:38 -0400

    update atributos privados

--- formacao/python/bytebank/src/python/br/com/tlmacedo/bytebank/conta.py
+++ formacao/python/bytebank/src/python/br/com/tlmacedo/bytebank/conta.py
@@ -11,25 +11,13 @@ class Conta:
 	def numero(self):
 		return self.__numero
 
-	@numero.setter
-	def numero(self, numero):
-		self.__numero = numero
-
 	@property
 	def titular(self):
 		return self.__titular
 
-	@titular.setter
-	def titular(self, titular):
-		self.__titular = titular
-
 	@property
 	def saldo(self):
 		return self.__saldo
-
-	@saldo.setter
-	def saldo(self, saldo):
-		self.__saldo = saldo
 
 	@property
 	def limite(self):
