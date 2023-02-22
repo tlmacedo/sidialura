e316b9e8da580f0ddb21c93a097bbf1577da9e83
tlmacedo <tl.macedo@hotmail.com>
2023-02-22 11:29:47 -0400

    Encapsulando

--- formacao/python/bytebank/src/python/br/com/tlmacedo/bytebank/conta.py
+++ formacao/python/bytebank/src/python/br/com/tlmacedo/bytebank/conta.py
@@ -1,17 +1,21 @@
 class Conta:
 
-    def __init__(self, numero, titular, saldo, limite):
-        print('Construindo objeto... {}'.format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+	def __init__(self, numero, titular, saldo, limite):
+		print('Construindo objeto... {}'.format(self))
+		self.__numero = numero
+		self.__titular = titular
+		self.__saldo = saldo
+		self.__limite = limite
 
-    def extrato(self):
-        print('Saldo de {} do titular: {}'.format(self.saldo, self.titular))
+	def extrato(self):
+		print('Saldo de {} do titular: {}'.format(self.__saldo, self.__titular))
 
-    def deposita(self, valor):
-        self.saldo += valor
+	def depositar(self, valor):
+		self.__saldo += valor
 
-    def saca(self, valor):
-        self.saldo -= valor
+	def sacar(self, valor):
+		self.__saldo -= valor
+
+	def transferir(self, valor, conta_destino):
+		self.sacar(valor)
+		conta_destino.depositar(valor)
